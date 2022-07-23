db1831306df82371fb1f683fbd7331e278bf9a62
Heache Bear <mrbeardad@qq.com>
2022-07-23 09:56:50 +0800

    fix catppuccin config

--- lua/user/config/catppuccin.lua
+++ lua/user/config/catppuccin.lua
@@ -1,28 +1,30 @@
 local M = {}
 
 M.config = function()
-	local catppuccin = require("catppuccin")
-	catppuccin.setup({
-		transparent_background = lvim.transparent_window,
-		term_colors = false,
-		styles = {
-			comments = "italic",
-			strings = "italic",
-			keywords = "italic",
-			functions = "italic",
-			numbers = "italic",
-		},
-		integrations = {
-			lsp_trouble = true,
-			nvimtree = {
-				transparent_panel = lvim.transparent_window,
-			},
-			which_key = true,
-			hop = true,
-			ts_rainbow = true,
-		},
-	})
-	vim.g.catppuccin_flavour = "macchiato"
+  local catppuccin = require("catppuccin")
+  catppuccin.setup({
+    transparent_background = lvim.transparent_window,
+    dim_inactive = {
+      enabled = true,
+    },
+    styles = {
+      comments = { "italic" },
+      strings = { "italic" },
+      keywords = { "italic" },
+      functions = { "italic" },
+      numbers = { "italic" },
+    },
+    integrations = {
+      lsp_trouble = true,
+      nvimtree = {
+        transparent_panel = lvim.transparent_window,
+      },
+      which_key = true,
+      ts_rainbow = true,
+      hop = true,
+    },
+  })
+  vim.g.catppuccin_flavour = "macchiato"
 end
 
 return M
