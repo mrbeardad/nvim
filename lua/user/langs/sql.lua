a2881a8d832b1bb7c89704de8713bd769085c798
Heache Bear <mrbeardad@qq.com>
2024-03-12 18:45:08 +0800

    feat(langs): add support for sql

---
+++ lua/user/langs/sql.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "sql" })
+    end,
+  },
+
+  -- require cgo
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       sqls = {},
+  --     },
+  --   },
+  -- },
+
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters_by_ft = {
+        sql = { "sqlfluff" },
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        sql = { "sqlfluff" },
+      },
+    },
+  },
+
+  {
+    "williamboman/mason.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "sqlfluff" })
+    end,
+  },
+}
