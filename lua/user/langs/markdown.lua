32657821d67b4e80f32b66760c2b914367d830bb
xionghaicheng <haicheng.xiong@maxjia.com>
2025-04-21 20:03:05 +0800

    fix(langs/markdown): disable marksman

--- lua/user/langs/markdown.lua
+++ lua/user/langs/markdown.lua
@@ -6,14 +6,14 @@ return {
     end,
   },
 
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        marksman = {},
-      },
-    },
-  },
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   opts = {
+  --     servers = {
+  --       marksman = {},
+  --     },
+  --   },
+  -- },
 
   {
     "mfussenegger/nvim-lint",
